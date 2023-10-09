8002b26c0b9b4f67821ac525b2055fc572a0763c
Adam Nicholson <adam.nicholson@sbs.com.au>
2023-10-09 16:49:46 +1000

    remove session domain

--- app/session.server.ts
+++ app/session.server.ts
@@ -15,8 +15,6 @@ const { getSession, commitSession, destroySession } =
     // a Cookie from `createCookie` or the CookieOptions to create one
     cookie: {
       name: "__session",
-      domain: "localhost",
-
       httpOnly: true,
       maxAge: 60,
       path: "/",
